e4a34146f6ce40842560f8b2ffb2df6bbb30f463
aprilsloan <729755869@qq.com>
2026-06-02 22:45:17 +0800

    Add + and - operators

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # xiaocc
 A small aarch64 c compiler modeled after [chibicc](https://github.com/rui314/chibicc/tree/main), the naming style of the project also follows the style of chibicc.
+
+I just try to use claude code to rewrite the project, hoping it will work.
--- main.c
+++ main.c
@@ -7,9 +7,29 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    char *p = argv[1];
+
     printf("    .global main\n");
     printf("main:\n");
-    printf("    mov x0, #%d\n", atoi(argv[1]));
+    printf("    mov x0, #%ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("    add x0, x0, #%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("    sub x0, x0, #%ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected character: '%c'\n", *p);
+        return 1;
+    }
+
     printf("    ret\n");
     return 0;
 }
