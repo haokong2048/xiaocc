5d517399b1bee1c0c0c94a2c1e88d8ca1d395a81
aprilsloan <729755869@qq.com>
2026-06-02 23:46:44 +0800

    Add "if" statement

--- codegen.c
+++ codegen.c
@@ -2,6 +2,11 @@
 
 static int depth;
 
+static int count(void) {
+    static int i = 1;
+    return i++;
+}
+
 static void push(void) {
     printf("    str x0, [sp, #-16]!\n");
     depth++;
@@ -92,6 +97,19 @@ static void gen_expr(Node *node) {
 
 static void gen_stmt(Node *node) {
     switch (node->kind) {
+    case ND_IF: {
+        int c = count();
+        gen_expr(node->cond);
+        printf("    cmp x0, #0\n");
+        printf("    b.eq .L.else.%d\n", c);
+        gen_stmt(node->then);
+        printf("    b .L.end.%d\n", c);
+        printf(".L.else.%d:\n", c);
+        if (node->els)
+            gen_stmt(node->els);
+        printf(".L.end.%d:\n", c);
+        return;
+    }
     case ND_BLOCK:
         for (Node *n = node->body; n; n = n->next)
             gen_stmt(n);
--- xiaocc.h
+++ xiaocc.h
@@ -75,6 +75,7 @@ typedef enum {
     ND_LE,        // <=
     ND_ASSIGN,    // =
     ND_RETURN,    // "return"
+    ND_IF,        // "if"
     ND_BLOCK,     // { ... }
     ND_EXPR_STMT, // 表达式语句
     ND_VAR,       // 变量
@@ -87,6 +88,11 @@ struct Node {
     Node *next;    // 下一个节点
     Node *lhs;     // 左操作数
     Node *rhs;     // 右操作数
+
+    // "if" 语句
+    Node *cond;
+    Node *then;
+    Node *els;
 
     // 块
     Node *body;
