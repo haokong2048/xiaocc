5acdd6c4d04b41dcbcf880239f66d06b6fe75ccf
aprilsloan <729755869@qq.com>
2026-06-09 23:00:43 +0800

    Add void type

--- xiaocc.h
+++ xiaocc.h
@@ -154,6 +154,7 @@ Obj *parse(Token *tok);
 //
 
 typedef enum {
+    TY_VOID,
     TY_CHAR,
     TY_SHORT,
     TY_INT,
@@ -202,6 +203,8 @@ struct Member {
     Token *name;
     int offset;
 };
+
+extern Type *ty_void;
 
 extern Type *ty_char;
 extern Type *ty_short;
