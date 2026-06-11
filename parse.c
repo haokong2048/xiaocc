43134e7729178244d89bcdb1080193284ee0a40b
aprilsloan <729755869@qq.com>
2026-06-12 01:48:37 +0800

    Allow extraneous comma at the end of enum or initializer list

--- parse.c
+++ parse.c
@@ -546,10 +546,28 @@ static Type *typename(Token **rest, Token *tok) {
     return abstract_declarator(rest, tok, ty);
 }
 
+static bool is_end(Token *tok) {
+    return equal(tok, "}") || (equal(tok, ",") && equal(tok->next, "}"));
+}
+
+static bool consume_end(Token **rest, Token *tok) {
+    if (equal(tok, "}")) {
+        *rest = tok->next;
+        return true;
+    }
+
+    if (equal(tok, ",") && equal(tok->next, "}")) {
+        *rest = tok->next->next;
+        return true;
+    }
+
+    return false;
+}
+
 // enum-specifier = ident? "{" enum-list? "}"
 //                | ident ("{" enum-list? "}")?
 //
-// enum-list      = ident ("=" num)? ("," ident ("=" num)?)*
+// enum-list      = ident ("=" num)? ("," ident ("=" num)?)* ","?
 static Type *enum_specifier(Token **rest, Token *tok) {
     Type *ty = enum_type();
 
@@ -575,7 +593,7 @@ static Type *enum_specifier(Token **rest, Token *tok) {
     // 读取枚举列表
     int i = 0;
     int val = 0;
-    while (!equal(tok, "}")) {
+    while (!consume_end(rest, tok)) {
         if (i++ > 0)
             tok = skip(tok, ",");
 
@@ -589,8 +607,6 @@ static Type *enum_specifier(Token **rest, Token *tok) {
         sc->enum_ty = ty;
         sc->enum_val = val++;
     }
-
-    *rest = tok->next;
 
     if (tag)
         push_tag_scope(tag, ty);
@@ -654,7 +670,7 @@ static int count_array_init_elements(Token *tok, Type *ty) {
     Initializer *dummy = new_initializer(ty->base, false);
     int i = 0;
 
-    for (; !equal(tok, "}"); i++) {
+    for (; !consume_end(&tok, tok); i++) {
         if (i > 0)
             tok = skip(tok, ",");
         initializer2(&tok, tok, dummy);
@@ -662,7 +678,7 @@ static int count_array_init_elements(Token *tok, Type *ty) {
     return i;
 }
 
-// array-initializer1 = "{" initializer ("," initializer)* "}"
+// array-initializer1 = "{" initializer ("," initializer)* ","? "}"
 static void array_initializer1(Token **rest, Token *tok, Initializer *init) {
     tok = skip(tok, "{");
 
@@ -671,7 +687,7 @@ static void array_initializer1(Token **rest, Token *tok, Initializer *init) {
         *init = *new_initializer(array_of(init->ty->base, len), false);
     }
 
-    for (int i = 0; !consume(rest, tok, "}"); i++) {
+    for (int i = 0; !consume_end(rest, tok); i++) {
         if (i > 0)
             tok = skip(tok, ",");
 
@@ -689,7 +705,7 @@ static void array_initializer2(Token **rest, Token *tok, Initializer *init) {
         *init = *new_initializer(array_of(init->ty->base, len), false);
     }
 
-    for (int i = 0; i < init->ty->array_len && !equal(tok, "}"); i++) {
+    for (int i = 0; i < init->ty->array_len && !is_end(tok); i++) {
         if (i > 0)
             tok = skip(tok, ",");
         initializer2(&tok, tok, init->children[i]);
@@ -702,19 +718,20 @@ static void union_initializer(Token **rest, Token *tok, Initializer *init) {
     // 且该初始化器初始化联合体的第一个成员。
     if (equal(tok, "{")) {
         initializer2(&tok, tok->next, init->children[0]);
+        consume(&tok, tok, ",");
         *rest = skip(tok, "}");
     } else {
         initializer2(rest, tok, init->children[0]);
     }
 }
 
-// struct-initializer1 = "{" initializer ("," initializer)* "}"
+// struct-initializer1 = "{" initializer ("," initializer)* ","? "}"
 static void struct_initializer1(Token **rest, Token *tok, Initializer *init) {
     tok = skip(tok, "{");
 
     Member *mem = init->ty->members;
 
-    while (!consume(rest, tok, "}")) {
+    while (!consume_end(rest, tok)) {
         if (mem != init->ty->members)
             tok = skip(tok, ",");
 
@@ -731,7 +748,7 @@ static void struct_initializer1(Token **rest, Token *tok, Initializer *init) {
 static void struct_initializer2(Token **rest, Token *tok, Initializer *init) {
     bool first = true;
 
-    for (Member *mem = init->ty->members; mem && !equal(tok, "}"); mem = mem->next) {
+    for (Member *mem = init->ty->members; mem && !is_end(tok); mem = mem->next) {
         if (!first)
             tok = skip(tok, ",");
         first = false;
