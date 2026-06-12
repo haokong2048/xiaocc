c9b6c09089d358987f4c0c5fc3ce5bd619e2f79d
aprilsloan <729755869@qq.com>
2026-06-13 01:17:17 +0800

    Add string hashmap

--- xiaocc.h
+++ xiaocc.h
@@ -39,6 +39,30 @@ typedef struct {
 char *format(char *fmt, ...);
 
 //
+// hashmap.c
+//
+
+typedef struct {
+    char *key;
+    int keylen;
+    void *val;
+} HashEntry;
+
+typedef struct {
+    HashEntry *buckets;
+    int capacity;
+    int used;
+} HashMap;
+
+void *hashmap_get(HashMap *map, char *key);
+void *hashmap_get2(HashMap *map, char *key, int keylen);
+void hashmap_put(HashMap *map, char *key, void *val);
+void hashmap_put2(HashMap *map, char *key, int keylen, void *val);
+void hashmap_delete(HashMap *map, char *key);
+void hashmap_delete2(HashMap *map, char *key, int keylen);
+void hashmap_test(void);
+
+//
 // tokenize.c
 //
 
